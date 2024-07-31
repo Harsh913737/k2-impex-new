79b45123d9aba126ceed5e5cba694d86fc202f5d
Harsh913737 <93814159+Harsh913737@users.noreply.github.com>
2024-07-31 18:13:38 +0530

    api

---
+++ api/Configs/server-config.js
@@ -0,0 +1,5 @@
+export const serverConfig = {
+  // appServerUrl: "http://localhost:8000/api",
+  appServerUrl: "https://ar-creations.in/clients/k2-impex/api",
+  // appServerUrl: "https://admin.k2impex.in/api",
+};
